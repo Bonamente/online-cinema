c2db0f200ba937dd9ed52945d03fbd4672aeee03
Bonamente <56506035@protonmail.com>
2023-05-09 12:47:55 +0500

    feat: add custom-slider-extended

--- src/components/customs/custom-slider-extended/types.tsx
+++ src/components/customs/custom-slider-extended/types.tsx
@@ -1,18 +1,34 @@
-export type CustomSliderProps = {
-  children: React.ReactNode[];
+export interface SwiperComponentProps {
   id: string | number;
+  children: React.ReactNode[];
   buttonsPosition: ButtonsPositionType;
-  lastSlide?: React.ReactNode;
   slidesPerView: SlidesPerViewType;
+  slidesPerGroup?: SlidesPerGroupType;
+  title?: string;
+  link?: string;
+  lastSlide?: React.ReactNode;
   spaceBetween?: number;
+  allowTouchMove?: boolean;
   showNavBtnsOnHoverOnly?: boolean;
+  hasButtons?: boolean;
+  hasButtonsBackground?: boolean;
+  hasShadow?: boolean;
+  centeredSlides?: boolean;
+  initialSlide?: number;
   isAutoplay?: boolean;
+  loop?: boolean;
   delay?: number;
   speed?: number;
   isTransitionTimingFnLinear?: boolean;
   isDirectionReversed?: boolean;
-};
+}
+
+export interface CustomSliderProps extends SwiperComponentProps {
+  title?: string;
+  link?: string;
+}
 
 export type SizeType = 'small' | 'default';
-export type ButtonsPositionType = 'inside' | 'outside';
+export type ButtonsPositionType = 'inside' | 'outside' | 'promo-large' | 'promo-small';
 export type SlidesPerViewType = number | 'auto';
+export type SlidesPerGroupType = number | 1;
